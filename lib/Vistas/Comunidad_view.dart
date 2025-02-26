b0d8230e556000b8239200993f062c522b1aa697
DavidDu2002 <122420478+DavidDu2002@users.noreply.github.com>
2025-02-26 18:50:56 -0500

    arreglo de pantallas principales
    arreglo

--- lib/Vistas/Comunidad_view.dart
+++ lib/Vistas/Comunidad_view.dart
@@ -19,6 +19,12 @@ class _MiembroComunidadState extends State<MiembroComunidad> {
         actions: [
           IconButton(
             onPressed: () {
+              _mostrarInfoApp(context);
+            },
+            icon: const Icon(Icons.info_outline),
+          ),
+          IconButton(
+            onPressed: () {
               logout(context);
             },
             icon: const Icon(Icons.logout),
@@ -44,7 +50,7 @@ class _MiembroComunidadState extends State<MiembroComunidad> {
               ),
               child: SizedBox(
                 height:
-                    MediaQuery.of(context).size.height * 0.5, // Altura reducida
+                    MediaQuery.of(context).size.height * 0.5, // Altura ajustada
                 width: MediaQuery.of(context).size.width *
                     0.9, // Mantengo el mismo ancho
                 child: Padding(
@@ -53,88 +59,38 @@ class _MiembroComunidadState extends State<MiembroComunidad> {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        // Título de la descripción con color diferente
+                        const SizedBox(height: 20),
+                        // Título para los servicios
                         const Text(
-                          'Descripción de la Aplicación',
+                          'Servicios ofrecidos',
                           style: TextStyle(
                             fontSize: 18,
                             fontWeight: FontWeight.bold,
                             color: Color(0xFF4A4A4A), // Gris oscuro
                           ),
                         ),
-                        const SizedBox(height: 10),
-                        // Descripción
-                        const Text(
-                          'Modelo de gestión de servicios que ayuda a los miembros de la comunidad a acceder a diversos servicios esenciales, con un enfoque en la organización, economía, y desarrollo comunitario.',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color:
-                                Colors.teal, // Color teal para la descripción
-                          ),
+                        const SizedBox(height: 20),
+                        // Botones para los servicios con separación
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            _crearBoton(
+                                context,
+                                'Técnicas de administración comunitaria',
+                                Icons.business),
+                            const SizedBox(
+                                width: 15), // Espacio entre los botones
+                            _crearBoton(
+                                context,
+                                'Administración de economía familiar',
+                                Icons.monetization_on),
+                          ],
                         ),
                         const SizedBox(height: 20),
-                        // Título para los servicios con color diferente
-                        const Text(
-                          'Servicios ofrecidos',
-                          style: TextStyle(
-                            fontSize: 18, // Tamaño de texto ajustado
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xFF4A4A4A), // Gris oscuro
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        // Lista de viñetas con iconos
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              children: const [
-                                Icon(Icons.business, color: Colors.teal),
-                                SizedBox(width: 10),
-                                Expanded(
-                                  child: Text(
-                                    'Técnicas de administración comunitaria (Manejo de costos fijos).',
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      color: Colors.teal,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            const SizedBox(height: 10),
-                            Row(
-                              children: const [
-                                Icon(Icons.monetization_on, color: Colors.teal),
-                                SizedBox(width: 10),
-                                Expanded(
-                                  child: Text(
-                                    'Administración de economía familiar.',
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      color: Colors.teal,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            const SizedBox(height: 10),
-                            Row(
-                              children: const [
-                                Icon(Icons.hotel, color: Colors.teal),
-                                SizedBox(width: 10),
-                                Expanded(
-                                  child: Text(
-                                    'Gestión turística.',
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      color: Colors.teal,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
+                        // Botón "Gestión turística" en el centro
+                        Center(
+                          child: _crearBoton(
+                              context, 'Gestión turística', Icons.hotel),
                         ),
                       ],
                     ),
@@ -148,6 +104,74 @@ class _MiembroComunidadState extends State<MiembroComunidad> {
     );
   }
 
+  // Método para crear los botones
+  Widget _crearBoton(BuildContext context, String texto, IconData icono) {
+    return Container(
+      width: 120, // Tamaño fijo más pequeño para los botones
+      height: 120, // Tamaño fijo más pequeño para los botones
+      decoration: BoxDecoration(
+        color: Colors.white, // Fondo blanco
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.grey,
+            blurRadius: 4,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          borderRadius: BorderRadius.circular(15),
+          onTap: () {
+            // Acción de navegación o detalle
+            // Aquí puedes agregar la lógica de cada servicio
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(icono,
+                    color: Colors.teal, size: 35), // Tamaño del ícono reducido
+                const SizedBox(height: 5),
+                Text(
+                  texto,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                      fontSize: 12,
+                      color: Colors.black), // Tamaño de texto reducido
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  // Método para mostrar la descripción adicional al presionar el icono de información
+  void _mostrarInfoApp(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Descripción de la Aplicación"),
+          content: const Text(
+              "Modelo de gestión de servicios que ayuda a los miembros de la comunidad a acceder a diversos servicios esenciales, con un enfoque en la organización, economía, y desarrollo comunitario."),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text("Cerrar"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  // Método para el cierre de sesión
   Future<void> logout(BuildContext context) async {
     await FirebaseAuth.instance.signOut();
     Navigator.pushReplacement(
--- lib/Vistas/Lideres_view.dart
+++ lib/Vistas/Lideres_view.dart
@@ -18,10 +18,16 @@ class _LiderComunidadState extends State<LiderComunidad> {
         actions: [
           IconButton(
             onPressed: () {
+              _mostrarInfoApp(context);
+            },
+            icon: const Icon(Icons.info_outline),
+          ),
+          IconButton(
+            onPressed: () {
               logout(context);
             },
             icon: const Icon(Icons.logout),
-          )
+          ),
         ],
       ),
       body: Container(
@@ -42,150 +48,93 @@ class _LiderComunidadState extends State<LiderComunidad> {
                 borderRadius: BorderRadius.circular(15),
               ),
               child: SizedBox(
-                height: MediaQuery.of(context).size.height *
-                    0.6, // Carta más pequeña
+                height: MediaQuery.of(context).size.height * 0.60,
                 width: MediaQuery.of(context).size.width * 0.9,
                 child: Padding(
                   padding: const EdgeInsets.all(20.0),
-                  child: SingleChildScrollView(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        // Título de la descripción con color negro
-                        const Text(
-                          'Descripción de la Aplicación',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black, // Color negro
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'Servicios ofrecidos',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Expanded(
+                        child: SingleChildScrollView(
+                          child: GridView.count(
+                            crossAxisCount: 2,
+                            crossAxisSpacing: 10,
+                            mainAxisSpacing: 10,
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            childAspectRatio: 1.2,
+                            children: [
+                              _crearBoton(
+                                  context, 'Adm. comunitaria', Icons.business),
+                              _crearBoton(context, 'Técnicas tributarias',
+                                  Icons.account_balance),
+                              _crearBoton(context, 'Liderazgo comunitario',
+                                  Icons.group_work),
+                              _crearBoton(
+                                  context, 'Derecho laboral', Icons.gavel),
+                              _crearBoton(context, 'Eco. familiar',
+                                  Icons.monetization_on),
+                              _crearBoton(
+                                  context, 'Gestión turística', Icons.hotel),
+                            ],
                           ),
                         ),
-                        const SizedBox(height: 10),
-                        // Descripción con el mismo tamaño de texto
-                        const Text(
-                          'Modelo de gestión de servicios que ayuda a los líderes comunitarios a identificar las necesidades de los miembros de las comunidades, promoviendo un enfoque integral para fortalecer la organización y la gestión comunitaria.',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.teal, // Color teal
-                          ),
-                        ),
-                        const SizedBox(height: 20),
-                        // Título para los servicios con color negro
-                        const Text(
-                          'Servicios ofrecidos',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black, // Color negro
-                          ),
-                        ),
-                        const SizedBox(height: 10),
-                        // Lista de viñetas con iconos
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              children: const [
-                                Icon(Icons.business, color: Colors.teal),
-                                SizedBox(width: 10),
-                                Expanded(
-                                  child: Text(
-                                    'Técnicas de administración comunitaria (Manejo de costos fijos).',
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      color: Colors.teal,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            const SizedBox(height: 10),
-                            Row(
-                              children: const [
-                                Icon(Icons.account_balance, color: Colors.teal),
-                                SizedBox(width: 10),
-                                Expanded(
-                                  child: Text(
-                                    'Técnicas tributarias.',
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      color: Colors.teal,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            const SizedBox(height: 10),
-                            Row(
-                              children: const [
-                                Icon(Icons.group_work, color: Colors.teal),
-                                SizedBox(width: 10),
-                                Expanded(
-                                  child: Text(
-                                    'Técnicas Administración de liderazgo comunitario.',
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      color: Colors.teal,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            const SizedBox(height: 10),
-                            Row(
-                              children: const [
-                                Icon(Icons.gavel, color: Colors.teal),
-                                SizedBox(width: 10),
-                                Expanded(
-                                  child: Text(
-                                    'Asesoramiento en derecho laboral y tributario.',
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      color: Colors.teal,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            const SizedBox(height: 10),
-                            Row(
-                              children: const [
-                                Icon(Icons.monetization_on, color: Colors.teal),
-                                SizedBox(width: 10),
-                                Expanded(
-                                  child: Text(
-                                    'Administración de economía familiar.',
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      color: Colors.teal,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                            const SizedBox(height: 10),
-                            Row(
-                              children: const [
-                                Icon(Icons.hotel, color: Colors.teal),
-                                SizedBox(width: 10),
-                                Expanded(
-                                  child: Text(
-                                    'Gestión turística.',
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      color: Colors.teal,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
               ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _crearBoton(BuildContext context, String texto, IconData icono) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.8),
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          borderRadius: BorderRadius.circular(15),
+          onTap: () {
+            // Acción de navegación o detalle
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(icono, color: Colors.teal, size: 40),
+                const SizedBox(height: 5),
+                Text(
+                  texto,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontSize: 14, color: Colors.black),
+                ),
+              ],
             ),
           ),
         ),
@@ -202,11 +151,30 @@ class _LiderComunidadState extends State<LiderComunidad> {
       ),
     );
   }
+
+  void _mostrarInfoApp(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Descripción de la Aplicación"),
+          content: const Text(
+              "Modelo de gestión de servicios que ayuda a los líderes comunitarios a identificar las necesidades de los miembros de las comunidades, promoviendo un enfoque integral para fortalecer la organización y la gestión comunitaria."),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text("Cerrar"),
+            ),
+          ],
+        );
+      },
+    );
+  }
 }
 
 void main() {
   runApp(MaterialApp(
-    debugShowCheckedModeBanner: false, // Esto elimina el banner de depuración
+    debugShowCheckedModeBanner: false,
     home: LiderComunidad(),
   ));
 }
